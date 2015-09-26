700197135ab4d7285001b778316a6ceff4b7d3cc
Marcin Lewandowski <marcin@radiokit.org>
2015-09-26 23:11:46 +0200

    Add __ prefixes in OAuth2

--- app/radiokit/auth/oauth2.js
+++ app/radiokit/auth/oauth2.js
@@ -1,8 +1,8 @@
 export default class OAuth2 {
   constructor(role) {
-    this.role = role;
-    this.baseUrl = "http://localhost:4000"; // FIXME
-    this.clientId = "123"; // FIXME
+    this.__role = role;
+    this.__baseUrl = "http://localhost:4000"; // FIXME
+    this.__clientId = "123"; // FIXME
   }
 
 
@@ -16,33 +16,33 @@ export default class OAuth2 {
 
       if(hashAsDict.hasOwnProperty("access_token")) {
         console.log("Authenticated");
-        this.accessToken = hashAsDict["access_token"];
-        this.cleanupLocationHash(hashAsDict, "access_token")
+        this.__accessToken = hashAsDict["access_token"];
+        this.__cleanupLocationHash(hashAsDict, "access_token")
         return this;
 
       } else if(hashAsDict.hasOwnProperty("error")) {
         console.warn("Authentication failed"); // TODO
-        this.cleanupLocationHash(hashAsDict, "error")
+        this.__cleanupLocationHash(hashAsDict, "error")
 
       } else {
-        this.redirectToAuthGateway();
+        this.__redirectToAuthGateway();
       }
 
     } else {
-      this.redirectToAuthGateway();
+      this.__redirectToAuthGateway();
     }
   }
 
 
-  cleanupLocationHash(hashAsDict, keyToDelete) {
+  __cleanupLocationHash(hashAsDict, keyToDelete) {
     delete hashAsDict[keyToDelete];
     window.location.hash = Object.keys(hashAsDict).map(x => encodeURIComponent(x) + "=" + encodeURIComponent(hashAsDict[x])).join("&")
   }
 
 
-  redirectToAuthGateway() {
+  __redirectToAuthGateway() {
     let redirectUrl = window.location.origin + window.location.pathname + window.location.search;
-    let authGatewayUrl = this.baseUrl + "/oauth2/authorize/" + encodeURIComponent(this.role) + "?response_type=token&client_id=" + encodeURIComponent(this.clientId) + "&redirect_uri=" + encodeURIComponent(redirectUrl);
+    let authGatewayUrl = this.__baseUrl + "/oauth2/authorize/" + encodeURIComponent(this.__role) + "?response_type=token&client_id=" + encodeURIComponent(this.__clientId) + "&redirect_uri=" + encodeURIComponent(redirectUrl);
 
     console.log("Redirecting to auth gateway: " + authGatewayUrl);
     window.location.assign(authGatewayUrl);
