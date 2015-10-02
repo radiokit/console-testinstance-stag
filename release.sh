00cf76fc8cd2b31ce773a1079904c8962030caa2
Marcin Lewandowski <marcin@radiokit.org>
2015-10-02 19:04:42 +0200

    Fixed final echo in release script

--- release.sh
+++ release.sh
@@ -12,4 +12,4 @@ BRANCH="gh-pages"
 REPO_URL="git@github.com:radiokit/site-console-$1.git"
 
 echo "Releasing $CURRENT_REVISION to '$1' environment..."
-git clone --depth=1 $REPO_URL $TMP_DIR -b $BRANCH && rm -rvf $TMP_DIR/* && cp -rv dist/shared/* $TMP_DIR/ && cp -rv dist/$1/* $TMP_DIR/ && npm run $1 && cd $TMP_DIR && git add -A && git commit -m "Releasing revision $CURRENT_REVISION" && git push origin $BRANCH && cd $CURRENT_DIR && rm -rvf $TMP_DIR && "Finished successfully"
+git clone --depth=1 $REPO_URL $TMP_DIR -b $BRANCH && rm -rvf $TMP_DIR/* && cp -rv dist/shared/* $TMP_DIR/ && cp -rv dist/$1/* $TMP_DIR/ && npm run $1 && cd $TMP_DIR && git add -A && git commit -m "Releasing revision $CURRENT_REVISION" && git push origin $BRANCH && cd $CURRENT_DIR && rm -rvf $TMP_DIR && echo -e "\n\nFinished successfully!\n"
