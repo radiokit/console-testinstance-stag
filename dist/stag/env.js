040064aa6d9e281c35ea2679327be93d94ef3848
Marcin Lewandowski <marcin@radiokit.org>
2015-10-02 19:03:11 +0200

    Fixed env vars

--- dist/stag/env.js
+++ dist/stag/env.js
@@ -1,8 +1,11 @@
 window.ENV = { 
-  auth: { clientId: "123", baseUrl: "http://stag.console.radiokitapp.org" }, 
+  auth: { 
+    clientId: "123", 
+    baseUrl: "https://radiokit-auth-stag.herokuapp.com"
+  }, 
   apps: { 
     "plumber" : { baseUrl: "https://radiokit-plumber-stag.herokuapp.com" },
     "auth" : { baseUrl: "https://radiokit-auth-stag.herokuapp.com" } 
   },
-  verbose: false
+  verbose: true
 };
