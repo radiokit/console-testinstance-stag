c0f7eca836b422060c911ddbbedbe5c5566a3b51
Marcin Lewandowski <marcin@radiokit.org>
2015-10-02 19:43:15 +0200

    Added robots.txt, added prod env config, added google analytics loading

---
+++ dist/prod/env.js
@@ -0,0 +1,14 @@
+window.ENV = { 
+  auth: { 
+    clientId: "aa4c4b4c4a82ab4c6b190c8db3870c5056444587e15f2f81765681990179c2960dcd1020717a5690", 
+    baseUrl: "https://auth.radiokitapp.org"
+  }, 
+  apps: { 
+    "plumber" : { baseUrl: "https://radiokit-plumber-prod.herokuapp.com" },
+    "auth" : { baseUrl: "https://radiokit-auth-prod.herokuapp.com" } 
+  },
+  external: {
+    googleAnalyticsID: "UA-68358393-2"
+  }
+  verbose: true
+};
--- dist/stag/env.js
+++ dist/stag/env.js
@@ -1,11 +1,14 @@
 window.ENV = { 
   auth: { 
-    clientId: "123", 
+    clientId: "6a2747066a4d0aa563d8c609f3811563dac32bc22307854a5e359933b192e0995496d5b8a918fc80", 
     baseUrl: "https://radiokit-auth-stag.herokuapp.com"
   }, 
   apps: { 
     "plumber" : { baseUrl: "https://radiokit-plumber-stag.herokuapp.com" },
     "auth" : { baseUrl: "https://radiokit-auth-stag.herokuapp.com" } 
   },
+  external: {
+    googleAnalyticsID: "UA-68358393-1"
+  }
   verbose: true
 };
