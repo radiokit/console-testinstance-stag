6ec3633cf4346cd214e87011d08bd4246b4d4860
Marcin Lewandowski <marcin@radiokit.org>
2015-10-23 18:33:12 +0200

    Use long-term caching while building the app

--- release.sh
+++ release.sh
@@ -11,5 +11,22 @@ TMP_DIR="/tmp/site-console-$1"
 BRANCH="gh-pages"
 REPO_URL="git@github.com:radiokit/site-console-$1.git"
 
+[ -e $TMP_DIR ] && rm -r $TMP_DIR
+
 echo "Releasing $CURRENT_REVISION to '$1' environment..."
-git clone --depth=1 $REPO_URL $TMP_DIR -b $BRANCH && rm -rvf $TMP_DIR/* && cp -rv dist/shared/* $TMP_DIR/ && cp -rv dist/$1/* $TMP_DIR/ && BUNDLE_FILENAME=$(basename `find $TMP_DIR/*.bundle.js`) && cat dist/shared/index.html | sed 's/PLACEHOLDER_BUNDLE/$BUNDLE_FILENAME/' && cp -v $TMP_DIR/index.html $TMP_DIR/404.html && npm run $1 && cd $TMP_DIR #&& git add -A && git commit -m "Releasing revision $CURRENT_REVISION" && git push origin $BRANCH && cd $CURRENT_DIR && rm -rvf $TMP_DIR && echo -e "\n\nFinished successfully!\n"
+git clone --depth=1 $REPO_URL $TMP_DIR -b $BRANCH || (echo "Error"; exit 2)
+rm -rvf $TMP_DIR/* || (echo "Error"; exit 2)
+cp -rv dist/shared/* $TMP_DIR/ || (echo "Error"; exit 2)
+cp -rv dist/$1/* $TMP_DIR/ || (echo "Error"; exit 2)
+cd $TMP_DIR || (echo "Error"; exit 2) 
+npm run $1 || (echo "Error"; exit 2)
+BUNDLE_FILENAME=$(basename `find $TMP_DIR/*.bundle.js`) || (echo "Error"; exit 2)
+cat dist/shared/index.html | sed 's/PLACEHOLDER_BUNDLE/$BUNDLE_FILENAME/' > $TMP_DIR/index.html || (echo "Error"; exit 2)
+cp -v $TMP_DIR/index.html $TMP_DIR/404.html || (echo "Error"; exit 2)
+cd $TMP_DIR || (echo "Error"; exit 2) 
+git add -A || (echo "Error"; exit 2)
+git commit -m "Releasing revision $CURRENT_REVISION" || (echo "Error"; exit 2)
+git push origin $BRANCH || (echo "Error"; exit 2)
+cd $CURRENT_DIR || (echo "Error"; exit 2)
+rm -rvf $TMP_DIR || (echo "Error"; exit 2)
+echo -e "\n\nFinished successfully!\n"
