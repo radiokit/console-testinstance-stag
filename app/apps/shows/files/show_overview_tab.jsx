e70ea595dbafab17d4597428a635d220afeb6493
Marcin Lewandowski <marcin@radiokit.org>
2015-11-14 20:28:19 +0100

    Display waveforms if present

--- app/apps/shows/files/show_overview_tab.jsx
+++ app/apps/shows/files/show_overview_tab.jsx
@@ -6,11 +6,69 @@ export default React.createClass({
   },
 
 
+  getInitialState: function() {
+    return {
+      currentVariantWaveform: null,
+      loadedVariantWaveform: false,
+      loadingError: false,
+    };
+  },
+
+
+  componentDidMount: function() {
+    this.loadVariantWaveform();
+  },
+
+
+  loadVariantWaveform: function() {
+    window.data
+      .query("vault", "Record.Variant")
+      .select("public_url")
+      .where("record_file_id", "eq", this.props.currentFile.get("id"))
+      .where("filter", "eq", "visualisation.waveform")
+      .on("error", () => {
+        if(this.isMounted()) {
+          this.setState({
+            loadingError: true
+          })
+        }
+      }).on("update", (_, response) => {
+        if(this.isMounted()) {
+
+          if(response.getData().count() != 0) {
+            this.setState({
+              currentVariantWaveform: response.getData().first(),
+              loadedVariantWaveform: true
+            });
+
+          } else {
+            this.setState({
+              loadedVariantWaveform: true,
+            })
+          }
+        }
+      }).fetch();
+  },
+
   render: function() {
-    return (
-      <div>
-        <img src="http://www.freddythunder.com/blog_images/wav2png2.png" style={{width: "100%", overflow: "hidden"}}/>  
-      </div>
-    );
+    if(this.state.loadedVariantWaveform) {
+      if(this.state.currentVariantWaveform) {
+        return (
+          <div style={{background: "#666" }}>
+            <img src={this.state.currentVariantWaveform.get("public_url")} style={{width: "100%"}} />
+          </div>
+        );
+      } else {
+        return <div/>;
+      }
+
+    } else {
+      return (
+        <div>
+          Loading...
+        </div>
+      );
+    }
+
   }
 });
