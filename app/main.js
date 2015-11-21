1d7eeb0838592aebb19b93818e8caf1a5dc0405a
Marcin Lewandowski <marcin@radiokit.org>
2015-11-21 17:59:52 +0100

    Updated plumber addresses

--- app/main.js
+++ app/main.js
@@ -58,7 +58,7 @@ function getEnv() {
     return {
       auth: { clientId: "123", baseUrl: "https://radiokit-auth-stag.herokuapp.com" },
       apps: {
-        "plumber" : { baseUrl: "https://radiokit-auth-stag.herokuapp.com" },
+        "plumber" : { baseUrl: "https://radiokit-plumber-stag.herokuapp.com" },
         "auth" : { baseUrl: "https://radiokit-auth-stag.herokuapp.com" },
         "vault" : { baseUrl: "https://radiokit-vault-stag.herokuapp.com" },
         "agenda": { baseUrl: "https://radiokit-agenda-stag.herokuapp.com" }
