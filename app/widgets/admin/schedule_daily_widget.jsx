caa44dacf692a3183dab329713fcf7bfabf78eed
Marcin Skotniczny <marcin@swmansion.com>
2016-03-08 19:06:02 +0100

    ScheduleDailyWidget refactor

--- app/widgets/admin/schedule_daily_widget.jsx
+++ app/widgets/admin/schedule_daily_widget.jsx
@@ -1,13 +1,137 @@
-import React from 'react';
+import React, { PropTypes } from 'react';
 import Immutable from 'immutable';
 import sprintf from 'tiny-sprintf';
 import Moment from 'moment';
+import classnames from 'classnames';
 
-import '../../assets/stylesheets/widgets/admin/schedule_daily.scss';
+import './schedule_daily_widget.scss';
+
+const hourType = PropTypes.oneOf([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]);
+
+function CalendarEntry({}) {
+
+}
+
+const CalendarRow = React.createClass({
+  propTypes: {
+    firstHour: hourType.isRequired,
+    hour: hourType.isRequired,
+    items: PropTypes.object.isRequired,
+    now: PropTypes.object.isRequired,
+    expanded: PropTypes.boolean,
+    onChangeExpansionState: PropTypes.func.isRequired,
+  },
+  onCollapse(e) {
+    this.props.onChangeExpansionState(this.props.hour, false)
+  },
+  onExpand(e) {
+    this.props.onChangeExpansionState(this.props.hour, true)
+  },
+  shouldComponentUpdate(nextProp, nextState) {
+    return !(
+      this.props.firstHour === nextProp.firstHour &&
+      this.props.hour === nextProp.hour &&
+      this.props.items === nextProp.items &&
+      this.props.now === nextProp.now &&
+      this.props.expanded === nextProp.expanded
+    );
+  },
+  render() {
+
+    const {hour, firstHour, items, now, expanded} = this.props;
+    let hourStart;
+    if(firstHour > 0 && hour < firstHour) {
+      hourStart = now.clone().startOf("day").hour(hour).add(1, "day");
+    } else {
+      hourStart = now.clone().startOf("day").hour(hour);
+    }
+    let hourStop = hourStart.clone().add(1, "hour");
+
+    let hourKlass;
+    if(now.clone().subtract(1, "hour").isAfter(hourStart)) {
+      hourKlass = "past";
+    } else if(now.isBetween(hourStart, hourStop)) {
+      hourKlass = "current";
+    } else {
+      hourKlass = null;
+    }
+
+    return (
+      <tr className={classnames("ScheduleDailyWidget-CalendarRow", hourKlass, {'ScheduleDailyWidget-CalendarRow--expanded': expanded})}>
+        <td className="ScheduleDailyWidget-CalendarRow-expandToggle">
+          <button className="btn btn-icon-toggle " onClick={expanded?this.onCollapse:this.onExpand}>
+            <i className={classnames('mdi', {'mdi-plus':!expanded,'mdi-minus':expanded})}  />
+          </button>
+        </td>
+
+        <td className="ScheduleDailyWidget-CalendarRow-hour">
+          {sprintf("%02s:00", hour)}
+        </td>
+
+        <td className="ScheduleDailyWidget-CalendarRow-items">
+          {items.filter((item) => {
+            return item.get("start_at").isBetween(hourStart, hourStop);
+
+          }).map((item) => {
+            if(item.get("stop_at").isAfter(item.get("start_at").clone().endOf("hour"))) {
+              return (
+                <div key={item.get("id")} className="item item-start" style={{top: (item.get("start_at").minutes() / 59 * 100) + "%", bottom: "0" }}>
+                  <header>
+                    {item.get("id")}
+                    / {item.get("start_at").format()}
+                    / {item.get("stop_at").format()}
+
+                  </header>
+                </div>
+              );
+
+            } else {
+              return (
+                <div key={item.get("id")} className="item item-overflow" style={{top: (item.get("start_at").minutes() / 59 * 100) + "%", bottom: (100 - item.get("stop_at").minutes() / 59 * 100) + "%" }}>
+                  <header>
+                    {item.get("id")}
+                    / {item.get("start_at").format()}
+                    / {item.get("stop_at").format()}
+                  </header>
+                </div>
+              );
+            }
+          })}
+
+
+          {this.props.items.filter((item) => {
+            return item.get("start_at").isBefore(hourStart)
+                   && item.get("stop_at").isAfter(hourStop);
+
+          }).map((item) => {
+            return (
+              <div key={item.get("id")} className="item item-stop" style={{bottom: "0", top: "-1px" }} />
+            );
+          })}
+
+
+          {this.props.items.filter((item) => {
+            return item.get("start_at").isBefore(hourStart)
+                   && item.get("stop_at").isBetween(hourStart, hourStop);
+
+          }).map((item) => {
+            return (
+              <div key={item.get("id")} className="item item-stop" style={{bottom: (100 - item.get("stop_at").minutes() / 59 * 100) + "%", top: "-1px" }} />
+            );
+          })}
+
+        </td>
+      </tr>
+    );
+  }
+});
+
+
+
 
 export default React.createClass({
   propTypes: {
-    firstHour: React.PropTypes.oneOf([0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23]).isRequired,
+    firstHour: hourType.isRequired,
     items: React.PropTypes.object.isRequired,
     now: React.PropTypes.object.isRequired,
   },
@@ -21,13 +145,17 @@ export default React.createClass({
     }
   },
 
-
-  onHourClick: function(hour) {
-    this.refs["hour" + hour].classList.toggle("expanded");
+  getInitialState: function() {
+    return {
+      expansionState: {}
+    }
   },
 
+  onChangeExpansionState(hour, value) {
+    this.setState({expansionState: {...this.state.expansionState, [hour]: value}});
+  },
 
-  render: function() {
+  render() {
     let hours;
 
     if(this.props.firstHour > 0) {
@@ -37,91 +165,18 @@ export default React.createClass({
     }
 
     return (
-      <table className="table table-banded table-hover widgets-admin-schedule-daily--container">
+      <table className="ScheduleDailyWidget table table-banded table-hover ">
         <tbody>
-          {hours.map((hour) => {
-            let hourStart;
-            if(this.props.firstHour > 0 && hour < this.props.firstHour) {
-              hourStart = this.props.now.clone().startOf("day").hour(hour).add(1, "day");
-            } else {
-              hourStart = this.props.now.clone().startOf("day").hour(hour);
-            }
-            let hourStop = hourStart.clone().add(1, "hour");
-
-            let hourKlass;
-            if(this.props.now.clone().subtract(1, "hour").isAfter(hourStart)) {
-              hourKlass = "past";
-            } else if(this.props.now.isBetween(hourStart, hourStop)) {
-              hourKlass = "current";
-            } else {
-              hourKlass = null;
-            }
-
+          {hours.map((hour)=> {
             return (
-              <tr key={hour} ref={"hour" + hour} className={hourKlass}>
-                <td className="text-right" className="expand-toggle">
-                  <a className="btn btn-icon-toggle toggle-on" onClick={this.onHourClick.bind(this, hour)}><i className="mdi mdi-plus" /></a>
-                  <a className="btn btn-icon-toggle toggle-off" onClick={this.onHourClick.bind(this, hour)}><i className="mdi mdi-minus" /></a>
-                </td>
-
-                <td className="text-right" className="hour">
-                  {sprintf("%02s:00", hour)}
-                </td>
-
-                <td className="items">
-                  {this.props.items.filter((item) => {
-                    return item.get("start_at").isBetween(hourStart, hourStop);
-
-                  }).map((item) => {
-                    if(item.get("stop_at").isAfter(item.get("start_at").clone().endOf("hour"))) {
-                      return (
-                        <div key={item.get("id")} className="item item-start" style={{top: (item.get("start_at").minutes() / 59 * 100) + "%", bottom: "0" }}>
-                          <header>
-                            {item.get("id")}
-                            / {item.get("start_at").format()}
-                            / {item.get("stop_at").format()}
-
-                          </header>
-                        </div>
-                      );
-
-                    } else {
-                      return (
-                        <div key={item.get("id")} className="item item-overflow" style={{top: (item.get("start_at").minutes() / 59 * 100) + "%", bottom: (100 - item.get("stop_at").minutes() / 59 * 100) + "%" }}>
-                          <header>
-                            {item.get("id")}
-                            / {item.get("start_at").format()}
-                            / {item.get("stop_at").format()}
-                          </header>
-                        </div>
-                      );
-                    }
-                  })}
-
-
-                  {this.props.items.filter((item) => {
-                    return item.get("start_at").isBefore(hourStart)
-                           && item.get("stop_at").isAfter(hourStop);
-
-                  }).map((item) => {
-                    return (
-                      <div key={item.get("id")} className="item item-stop" style={{bottom: "0", top: "-1px" }} />
-                    );
-                  })}
-
-
-                  {this.props.items.filter((item) => {
-                    return item.get("start_at").isBefore(hourStart)
-                           && item.get("stop_at").isBetween(hourStart, hourStop);
-
-                  }).map((item) => {
-                    return (
-                      <div key={item.get("id")} className="item item-stop" style={{bottom: (100 - item.get("stop_at").minutes() / 59 * 100) + "%", top: "-1px" }} />
-                    );
-                  })}
-
-                </td>
-              </tr>
+              <CalendarRow
+                key={hour}
+                hour={hour}
+                firstHour={this.props.firstHour}
+                now={this.props.now}
+                items={this.props.items}
+                onChangeExpansionState={this.onChangeExpansionState}
+                expanded={this.state.expansionState[hour]}/>
             );
           })}
         </tbody>
