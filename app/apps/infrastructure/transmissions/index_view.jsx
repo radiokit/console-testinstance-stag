209cbbb404dae1253fe5b156c84333aa5de4e3dd
Marcin Lewandowski <marcin@radiokit.org>
2016-03-09 14:38:34 +0100

    [Infrastructures] Do not fetch obsolete client nodes in infrastructure -> transmissions view

--- app/apps/infrastructure/transmissions/index_view.jsx
+++ app/apps/infrastructure/transmissions/index_view.jsx
@@ -5,19 +5,12 @@ import Index from '../../../widgets/admin/crud/index_widget.jsx';
 export default React.createClass({
   modifyIndexQuery: function(query) {
     return query
-    .select("audio_interface", "client_node")
-    .joins("audio_interface")
-    .joins("client_node");
+    .joins("audio_interface");
   },
 
 
   buildAttributes: function() {
     return {
-      device_name: { renderer: "string", valueFunc: (record) => {
-        if(record.has("client_node") && record.get("client_node")) {
-          return record.get("client_node").get("name") }
-        }
-      },
       audio_interface_name: { renderer: "string", valueFunc: (record) => {
         if(record.has("audio_interface") && record.get("audio_interface")) {
           return record.get("audio_interface").get("name") }
