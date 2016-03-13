78fcebdf8b2b0b4e91ce129fcd2840eb8c2223cd
Marcin Lewandowski <marcin@radiokit.org>
2016-03-13 16:50:08 +0100

    [Electron] Added missing locales

--- app/locales/en/apps/electron.js
+++ app/locales/en/apps/electron.js
@@ -20,6 +20,32 @@ export default {
       },
 
       devices: {
+        show: {
+          header: "Audio interfaces",
+          tabs: {
+            headers: {
+              audio_interface: "Audio interfaces",
+            },
+
+            body: {
+              audio_interface: {
+                table: {
+                  index: {
+                    table: {
+                      header: {
+                        name: "Name",
+                        os_name: "Name in the OS",
+                        direction: "Direction",
+                        transmission_enabled: "Transmission",
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+
         index: {
           header: "Audio Devices",
           actions: {
