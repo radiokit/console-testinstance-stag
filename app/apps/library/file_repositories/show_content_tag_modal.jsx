66321edb4ee96fad62a97884a43597dc18dd64f5
Marcin Adamczyk <marcin.adamczyk@swmansion.com>
2016-04-06 14:34:28 +0200

    create 3-state Checkbox component for file tag management

--- app/apps/library/file_repositories/show_content_tag_modal.jsx
+++ app/apps/library/file_repositories/show_content_tag_modal.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import Translate from 'react-translate-component';
+import _ from 'lodash';
+import ReactDOM from 'react-dom';
 
 import ModalForEach from '../../../widgets/admin/modal_foreach_widget.jsx';
 
-export default React.createClass({
+const ShowContentTagModal = React.createClass({
 
   propTypes: {
     selectedRecordIds: React.PropTypes.object.isRequired,
@@ -14,11 +16,48 @@ export default React.createClass({
     return {
       index: 0,
       selectedTagIds:[],
+      deselectedTagIds:[],
+      filesData: null
     }
+  },
+
+  selectTagId(tagId) {
+    console.log("selected tag " + tagId);
+    let selectedTagIds = this.state.selectedTagIds;
+    selectedTagIds.push(tagId);
+    this.setState({
+      selectedTagIds: selectedTagIds,
+      deselectedTagIds: _.pull(this.state.deselectedTagIds, tagId),
+    });
+  },
+
+  deselectTagId(tagId) {
+    console.log("deselected tag " + tagId);
+    let deselectedTagIds = this.state.deselectedTagIds;
+    deselectedTagIds.push(tagId);
+    this.setState({
+      selectedTagIds: _.pull(this.state.selectedTagIds, tagId),
+      deselectedTagIds: deselectedTagIds,
+    });
+  },
+
+  resetTagId(tagId) {
+    console.log("reseted tag " + tagId);
+    this.setState({
+      selectedTagIds: _.pull(this.state.selectedTagIds, tagId),
+      deselectedTagIds: _.pull(this.state.deselectedTagIds, tagId),
+    });
   },
 
   show() {
     this.refs.modal.show();
+  },
+
+  getTagStatus(tagId) {
+    return {
+      selected: true,
+      indeterminate: true,
+    };
   },
 
   onTagAppliedSucess(record) {
@@ -36,6 +75,10 @@ export default React.createClass({
         <div>
           <ul className="list">
             {category.tag_items && _.sortBy(category.tag_items,'name').map((tag) => {
+
+                let onTagSelected = this.selectTagId.bind(null, tag.id);
+                let onTagDeselected = this.deselectTagId.bind(null, tag.id);
+                let onTagRestored = this.resetTagId.bind(null, tag.id);
                 return (
                   <li key={ tag.id }>
                     <div className="card-head card-head-xs">
@@ -43,7 +86,11 @@ export default React.createClass({
                         { tag.name }
                       </header>
                       <div className="tools">
-                        <input type="checkbox" />
+                        <Checkbox
+                          onSelected={onTagSelected}
+                          onDeselected={onTagDeselected}
+                          onRestore={onTagRestored}
+                          indeterminate={this.getTagStatus(tag.id).indeterminate} />
                       </div>
                     </div>
                   </li>
@@ -114,3 +161,67 @@ export default React.createClass({
     );
   }
 });
+
+const Checkbox = React.createClass({
+
+  propTypes: {
+    checked: React.PropTypes.bool,
+    indeterminate: React.PropTypes.bool,
+    onSelected: React.PropTypes.func,
+    onDeselected: React.PropTypes.func,
+    onRestore: React.PropTypes.func
+  },
+
+  getInitialState() {
+    return {
+      checked: false,
+      indeterminate: false,
+    }
+  },
+
+  onChange(e) {
+    if(this.state.checked){
+      this.props.onDeselected();
+      this.setState({
+        checked: false,
+        indeterminate: false
+      });
+    }
+    else{
+      if(this.props.indeterminate && !this.state.indeterminate){
+        this.props.onRestore();
+        this.setState({
+          checked: false,
+          indeterminate: true,
+        });
+      } else {
+        this.props.onSelected();
+        this.setState({
+          checked: true,
+          indeterminate: false
+        });
+      }
+    }
+  },
+
+  componentDidMount() {
+    if (this.state.indeterminate) {
+      this.setIndeterminate(true);
+    }
+  },
+
+  componentDidUpdate(previousProps) {
+    this.setIndeterminate(this.state.indeterminate);
+  },
+
+  setIndeterminate(indeterminate) {
+    const node = ReactDOM.findDOMNode(this);
+    node.indeterminate = indeterminate;
+  },
+
+  render() {
+    return <input type="checkbox" checked={this.state.checked} onChange={this.onChange}/>;
+  }
+});
+
+export default ShowContentTagModal;
