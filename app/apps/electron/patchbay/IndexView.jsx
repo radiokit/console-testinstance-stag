d02073e97b75c46bc82b090519653509e9343f1a
Marcin Lewandowski <marcin@radiokit.org>
2016-04-11 12:34:32 +0200

    [Electron.Patchbay] Removed obsolete components

--- app/apps/electron/patchbay/IndexView.jsx
+++ app/apps/electron/patchbay/IndexView.jsx
@@ -7,7 +7,6 @@ import Section from '../../../widgets/admin/section_widget.jsx';
 import Card from '../../../widgets/admin/card_widget.jsx';
 
 import IndexDiagramPartial from './IndexDiagramPartial.jsx';
-import IndexTablePartial from './IndexTablePartial.jsx';
 
 Counterpart.registerTranslations("en", require('./IndexView.locale.en.js'));
 Counterpart.registerTranslations("pl", require('./IndexView.locale.pl.js'));
@@ -16,7 +15,6 @@ Counterpart.registerTranslations("pl", require('./IndexView.locale.pl.js'));
 export default React.createClass({
   buildTabs: function() {
     return {
-      // table:    { element: IndexTablePartial, props: { contentPrefix: "apps.electron.patchbay.table" } },
       diagram:  { element: IndexDiagramPartial, props: { contentPrefix: "apps.electron.patchbay.diagram" } },
     }
   },
