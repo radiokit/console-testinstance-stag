f545c36e6f9dbac1ec400f20e26858093ebeb646
Marcin Lewandowski <marcin@radiokit.org>
2016-04-11 11:57:47 +0200

    Use SIMPLE compilation mode in google closure compiler

--- webpack.prod.config.js
+++ webpack.prod.config.js
@@ -25,7 +25,7 @@ var config = {
       compiler: {
         language_in: 'ECMASCRIPT6',
         language_out: 'ECMASCRIPT5',
-        compilation_level: 'ADVANCED'
+        compilation_level: 'SIMPLE'
       },
       concurrency: 3,
     })
