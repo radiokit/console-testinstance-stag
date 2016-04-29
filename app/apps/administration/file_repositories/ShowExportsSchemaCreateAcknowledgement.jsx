60aad0fc7866e3284cf5d77817723abaa8e2682c
Marcin Lewandowski <marcin@radiokit.org>
2016-04-29 16:22:36 +0200

    DPT-26 Fixed mixcloud authentication link

--- app/apps/administration/file_repositories/ShowExportsSchemaCreateAcknowledgement.jsx
+++ app/apps/administration/file_repositories/ShowExportsSchemaCreateAcknowledgement.jsx
@@ -23,7 +23,7 @@ export default React.createClass({
         <div className="row style-accent">
           <div className="col-md-12 text-center">
             <div className="btn-group btn-group-justified small-padding" role="group">
-              <a className="btn btn-default-bright" href={`${window.data.options.apps.vault.baseUrl}/api/connector/v1.0/mixcloud/ask?record_repository_id=${encodeURIComponent(this.props.record.get("id"))}`}>
+              <a className="btn btn-default-bright" href={`${window.data.options.apps.vault.baseUrl}/api/connector/v1.0/mixcloud/ask?data_export_schema_id=${encodeURIComponent(this.props.record.get("id"))}`}>
                 <Translate component="span" content={`${this.props.contentPrefix}.acknowledgement.instructions.mixcloud.action_open`} />
               </a>
             </div>
