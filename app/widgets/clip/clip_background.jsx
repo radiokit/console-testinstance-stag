7d69f06570d9b6f9ecfa1870d32dc80d51a02e7c
Artur Kulig <artur.kulig@swmansion.com>
2016-05-05 15:35:11 +0200

    code style

--- app/widgets/clip/clip_background.jsx
+++ app/widgets/clip/clip_background.jsx
@@ -26,7 +26,7 @@ const ClipBackground = props => {
   const imageUrl = (sufficientImage || anyImage || Map()).get('url', '');
 
   const rootProps = {
-    style: ({
+    style: {
       position: 'relative',
       width,
       height,
@@ -34,22 +34,22 @@ const ClipBackground = props => {
       backgroundImage: `url(${imageUrl})`,
       backgroundSize: `${imageWidth + 1}px ${height}px`,
       backgroundPosition: `${Math.floor(imageWidth * (offsetStart / clipLength) * -1 - 1)}px 0`,
-    }),
+    },
     className: 'ClipBackground',
   };
 
-  const vignetteStyle1 = ({
+  const vignetteStyle1 = {
     position: 'absolute',
     background: 'linear-gradient(to bottom, rgba(0,0,0,0.15) 0%,rgba(0,0,0,0) 50%,rgba(0,0,0,0.15) 100%)',
     top: 0,
     left: 0,
     bottom: 0,
     right: 0,
-  });
-  const vignetteStyle2 = ({
+  };
+  const vignetteStyle2 = {
     ...vignetteStyle1,
     background: 'linear-gradient(to right, rgba(0,0,0,0.15) 0%,rgba(0,0,0,0) 20%,rgba(0,0,0,0) 79%,rgba(0,0,0,0.15) 100%)',
-  });
+  };
 
   return (
     <div {...rootProps}>
