64af5c7efb8d85257fd6b8d42d924b1d78350e67
lucek <lukasz.odziewa@gmail.com>
2016-05-13 13:01:55 +0200

    RoutingDiagramCanvas Eslint

--- app/apps/electron/patchbay/RoutingDiagramCanvas.jsx
+++ app/apps/electron/patchbay/RoutingDiagramCanvas.jsx
@@ -2,8 +2,6 @@ import React from 'react';
 import Immutable from 'immutable';
 
 import Toolbar from '../../../widgets/admin/toolbar_widget.jsx';
-import ToolbarButton from '../../../widgets/admin/toolbar_button_widget.jsx';
-import ToolbarGroup from '../../../widgets/admin/toolbar_group_widget.jsx';
 import DevicesToolbar from './DevicesToolbar.jsx';
 
 
@@ -20,7 +18,7 @@ export default React.createClass({
   },
 
 
-  getInitialState: function() {
+  getInitialState() {
     return {
       selectedAudioInterface: null,
       selectedClient: null,
@@ -29,39 +27,40 @@ export default React.createClass({
   },
 
 
-  getLinkRulesOfAudioInterface: function(audioInterface) {
-    return this.props.linkRules.filter((linkRule) => {
-      return (
-        linkRule.get("source_audio_interface_id") === audioInterface.get("id")
-      );
-    });
-  },
-
-
-  onClientDragMove: function(client, x, y) {
-    this.clientsCoordinates = this.clientsCoordinates.set(client.get("id"), new Immutable.Map({x: x, y: y}));
+  onClientDragMove(client, x, y) {
+    this.clientsCoordinates =
+      this.clientsCoordinates.set(client.get('id'), new Immutable.Map({ x, y }));
     this.forceUpdate();
   },
 
 
-  onClientDragStop: function(client, x, y) {
-    if(this.props.onClientDragStop) {
+  onClientDragStop(client, x, y) {
+    if (this.props.onClientDragStop) {
       this.props.onClientDragStop(client, x, y);
     }
   },
 
 
-  onAudioInterfaceClick: function(audioInterface) {
-    if(this.state.selectedAudioInterface === null) {
+  getLinkRulesOfAudioInterface(audioInterface) {
+    return this.props.linkRules.filter((linkRule) => {
+      return (
+        linkRule.get('source_audio_interface_id') === audioInterface.get('id')
+      );
+    });
+  },
+
+
+  onAudioInterfaceClick(audioInterface) {
+    if (this.state.selectedAudioInterface === null) {
       this.setState({
         selectedAudioInterface: audioInterface,
       });
     } else {
-      if(this.state.selectedAudioInterface.get("direction") !== audioInterface.get("direction")) {
+      if (this.state.selectedAudioInterface.get('direction') !== audioInterface.get('direction')) {
         let sourceAudioInterface;
         let destinationAudioInterface;
 
-        if(this.state.selectedAudioInterface.get("direction") === "capture") {
+        if (this.state.selectedAudioInterface.get('direction') === 'capture') {
           sourceAudioInterface = this.state.selectedAudioInterface;
           destinationAudioInterface = audioInterface;
         } else {
@@ -73,13 +72,12 @@ export default React.createClass({
           selectedAudioInterface: null,
         }, () => {
           window.data
-            .record("plumber", "Config.Routing.LinkRule")
+            .record('plumber', 'Config.Routing.LinkRule')
             .create({
-              source_audio_interface_id: sourceAudioInterface.get("id"),
-              destination_audio_interface_id: destinationAudioInterface.get("id"),
+              source_audio_interface_id: sourceAudioInterface.get('id'),
+              destination_audio_interface_id: destinationAudioInterface.get('id'),
             });
         });
-
       } else {
         this.setState({
           selectedAudioInterface: audioInterface,
@@ -89,91 +87,86 @@ export default React.createClass({
   },
 
 
-  onLinkRuleClick: function(linkRule) {
-    if(this.state.selectedLinkRule) {
-      if(this.state.selectedLinkRule.get("id") === linkRule.get("id")) {
+  onLinkRuleClick(linkRule) {
+    if (this.state.selectedLinkRule) {
+      if (this.state.selectedLinkRule.get('id') === linkRule.get('id')) {
         this.setState({
           selectedLinkRule: null,
         });
-
       } else {
         this.setState({
           selectedLinkRule: linkRule,
         });
       }
-
     } else {
       this.setState({
         selectedClient: null,
-        selectedLinkRule: linkRule
+        selectedLinkRule: linkRule,
       });
     }
   },
 
 
-  onClientBoxClick: function(client) {
-    if(this.state.selectedClient) {
-      if(this.state.selectedClient.get("id") === client.get("id")) {
+  onClientBoxClick(client) {
+    if (this.state.selectedClient) {
+      if (this.state.selectedClient.get('id') === client.get('id')) {
         this.setState({
           selectedClient: null,
         });
-
       } else {
         this.setState({
           selectedClient: client,
         });
       }
-
     } else {
       this.setState({
         selectedLinkRule: null,
-        selectedClient: client
+        selectedClient: client,
       });
     }
   },
 
 
-  componentWillMount: function() {
+  componentWillMount() {
     // We do not use state as it's updates are not not happening immediately
     // which results in sluggish UI
     this.clientsCoordinates = new Immutable.Map();
   },
 
 
-  componentWillUnount: function() {
+  getSelectedRecord() {
+    const selectedRecord = { record: null, model: 'no-model', id: 'no-id' };
+
+    if (this.state.selectedLinkRule || this.state.selectedClient) {
+      if (this.state.selectedLinkRule) {
+        selectedRecord.record = this.state.selectedLinkRule;
+        selectedRecord.id = this.state.selectedLinkRule.get('id');
+        selectedRecord.model = 'Config.Routing.LinkRule';
+      } else {
+        selectedRecord.record = this.state.selectedClient;
+        selectedRecord.id = this.state.selectedClient.get('id');
+        selectedRecord.model = 'Client.Standalone';
+      }
+    }
+
+    return selectedRecord;
+  },
+
+
+  componentWillUnount() {
     delete this.clientsCoordinates;
   },
 
 
-  getSelectedRecord: function() {
-    var selectedRecord = {"record": null, "model": "no-model", "id": "no-id"};
-
-    if(this.state.selectedLinkRule || this.state.selectedClient) {
-      if(this.state.selectedLinkRule){
-        selectedRecord["record"] = this.state.selectedLinkRule
-        selectedRecord["id"]     = this.state.selectedLinkRule.get('id')
-        selectedRecord["model"]  = "Config.Routing.LinkRule"
-      } else {
-        selectedRecord["record"] = this.state.selectedClient
-        selectedRecord["id"]     = this.state.selectedClient.get('id'),
-        selectedRecord["model"]  = "Client.Standalone"
-      };
-    };
-
-    return(selectedRecord);
-  },
-
-
-  render: function() {
-    console.log("RENDER");
+  render() {
     let filteredLinkRules = this.props.linkRules
       .filter((linkRule) => {
         return ( // FIXME should be obsolete if backend was returing limited data
-          this.props.audioInterfaces.find((audioInterface) => { return audioInterface.get("id") === linkRule.get("source_audio_interface_id"); }) ||
-          this.props.audioInterfaces.find((audioInterface) => { return audioInterface.get("id") === linkRule.get("destination_audio_interface_id"); })
+          this.props.audioInterfaces.find((audioInterface) => { return audioInterface.get('id') === linkRule.get('source_audio_interface_id'); }) ||
+          this.props.audioInterfaces.find((audioInterface) => { return audioInterface.get('id') === linkRule.get('destination_audio_interface_id'); })
         ) && (
-          linkRule.get("source_audio_interface_id") !== null &&
-          linkRule.get("destination_audio_interface_id") !== null
+          linkRule.get('source_audio_interface_id') !== null &&
+          linkRule.get('destination_audio_interface_id') !== null
         );
       });
 
@@ -182,7 +175,7 @@ export default React.createClass({
     return (
       <div>
         <Toolbar>
-          <DevicesToolbar 
+          <DevicesToolbar
             selectedRecord={this.getSelectedRecord()}
           />
         </Toolbar>
@@ -197,14 +190,16 @@ export default React.createClass({
             onClientDragMove={this.onClientDragMove}
             onClientDragStop={this.onClientDragStop}
             onClientBoxClick={this.onClientBoxClick}
-            selectedClient={this.state.selectedClient} />
+            selectedClient={this.state.selectedClient}
+          />
           <RoutingDiagramLinkRuleLayer
             clients={this.props.clients}
             audioInterfaces={this.props.audioInterfaces}
             linkRules={filteredLinkRules}
             selectedLinkRule={this.state.selectedLinkRule}
             onLinkRuleClick={this.onLinkRuleClick}
-            clientsCoordinates={this.clientsCoordinates} />
+            clientsCoordinates={this.clientsCoordinates}
+          />
         </svg>
       </div>
     );
