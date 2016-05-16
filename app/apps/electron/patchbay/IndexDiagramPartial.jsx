506b3cdd716450e07c12e9fc889f18433139a925
lucek <lukasz.odziewa@gmail.com>
2016-05-16 13:49:59 +0200

    IndexDiagramPartial Eslint

--- app/apps/electron/patchbay/IndexDiagramPartial.jsx
+++ app/apps/electron/patchbay/IndexDiagramPartial.jsx
@@ -6,36 +6,35 @@ import RoutingDiagramCanvas from './RoutingDiagramCanvas.jsx';
 
 
 export default React.createClass({
+  propTypes: {
+    contentPrefix: React.PropTypes.string.isRequired,
+  },
+
   contextTypes: {
     currentUserAccount: React.PropTypes.object.isRequired,
   },
 
 
-  propTypes: {
-    contentPrefix: React.PropTypes.string.isRequired,
-  },
-
-
-  getInitialState: function() {
+  getInitialState() {
     return {
       loadedClients: null,
       loadedAudioInterfaces: null,
       loadedLinkRules: null,
-    }
+    };
   },
 
 
-  loadClients: function() {
-    if(!this.clientsQuery) {
+  loadClients() {
+    if (!this.clientsQuery) {
       this.clientsQuery = window.data
-        .query("auth", "Client.Standalone")
-        .select("id", "name", "extra")
-        .where("application", "eq", "electron")
-        .where("account_id", "eq", this.context.currentUserAccount.get("id"))
-        .on("fetch", (_event, _query, data) => {
-          if(this.isMounted()) {
+        .query('auth', 'Client.Standalone')
+        .select('id', 'name', 'extra')
+        .where('application', 'eq', 'electron')
+        .where('account_id', 'eq', this.context.currentUserAccount.get('id'))
+        .on('fetch', (_event, _query, data) => {
+          if (this.isMounted()) {
             this.setState({
-              loadedClients: this.buildClientsWithExtra(data)
+              loadedClients: this.buildClientsWithExtra(data),
             }, () => {
               this.loadAudioInterfaces();
             });
@@ -46,65 +45,64 @@ export default React.createClass({
   },
 
 
-  buildClientsWithExtra: function(clients) {
+  buildClientsWithExtra(clients) {
     // TODO improve algorithm that will append X/Y positions to clients
     // if it's necessary so it does not assign 0, 0 all the time
     return clients.map((client) => {
-      if(!this.clientHasExtra(client)) {
+      if (!this.clientHasExtra(client)) {
         return this.setDefaultClientExtra(client);
-      } else {
-        return client;
       }
+
+      return client;
     });
   },
 
 
-  clientHasExtra: function(client) {
+  clientHasExtra(client) {
     return (
-      client.has("extra") && client.get("extra") !== null &&
-      client.get("extra").has("electron") && client.get("extra").get("electron") !== null &&
-      client.get("extra").get("electron").has("diagram") && client.get("extra").get("electron").get("diagram") !== null &&
-      client.get("extra").get("electron").get("diagram").has("x") && client.get("extra").get("electron").get("diagram").get("x") !== null &&
-      client.get("extra").get("electron").get("diagram").has("x") && client.get("extra").get("electron").get("diagram").get("y") !== null
+      client.has('extra') && client.get('extra') !== null &&
+      client.get('extra').has('electron') && client.get('extra').get('electron') !== null &&
+      client.get('extra').get('electron').has('diagram') && client.get('extra').get('electron').get('diagram') !== null &&
+      client.get('extra').get('electron').get('diagram').has('x') && client.get('extra').get('electron').get('diagram').get('x') !== null &&
+      client.get('extra').get('electron').get('diagram').has('x') && client.get('extra').get('electron').get('diagram').get('y') !== null
     );
   },
 
 
-  buildDefaultClientExtra: function() {
+  buildDefaultClientExtra() {
     return Immutable.fromJS({
       electron: {
         diagram: {
-           x: 0,
-           y: 0,
-         }
-       }
-     }
+          x: 0,
+          y: 0,
+        },
+      },
+    }
    );
   },
 
 
-  setDefaultClientExtra: function(client) {
-    if(!client.has("extra") || client.get("extra") === null) {
-      return client.set("extra", this.buildDefaultClientExtra());
-
-    } else {
-      return client.set("extra", client.get("extra").merge(this.buildDefaultClientExtra()));
+  setDefaultClientExtra(client) {
+    if (!client.has('extra') || client.get('extra') === null) {
+      return client.set('extra', this.buildDefaultClientExtra());
     }
+
+    return client.set('extra', client.get('extra').merge(this.buildDefaultClientExtra()));
   },
 
 
-  loadAudioInterfaces: function() {
-    if(!this.audioInterfacesQuery) {
-      let clientsGlobalIDs = this.state.loadedClients.map((client) => { return Data.buildRecordGlobalID("auth", "Client.Standalone", client.get("id")); }).toJS();
-      let clientsCondition = ["references", "din", "owner"].concat(clientsGlobalIDs)
+  loadAudioInterfaces() {
+    if (!this.audioInterfacesQuery) {
+      let clientsGlobalIDs = this.state.loadedClients.map((client) => { return Data.buildRecordGlobalID('auth', 'Client.Standalone', client.get('id')); }).toJS();
+      let clientsCondition = ['references', 'din', 'owner'].concat(clientsGlobalIDs)
 
       this.audioInterfacesQuery = window.data
-        .query("plumber", "Resource.Architecture.AudioInterface")
-        .select("id", "name", "direction", "references")
-        .order("name", "asc")
+        .query('plumber', 'Resource.Architecture.AudioInterface')
+        .select('id', 'name', 'direction', 'references')
+        .order('name', 'asc')
         // .where.apply(this, clientsCondition) // FIXME
-        .on("fetch", (_event, _query, data) => {
-          if(this.isMounted()) {
+        .on('fetch', (_event, _query, data) => {
+          if (this.isMounted()) {
             this.setState({
               loadedAudioInterfaces: data
             }, () => {
@@ -117,13 +115,13 @@ export default React.createClass({
   },
 
 
-  loadLinkRules: function() {
-    if(!this.linkRulesQuery) {
+  loadLinkRules() {
+    if (!this.linkRulesQuery) {
       this.linkRulesQuery = window.data
-        .query("plumber", "Config.Routing.LinkRule")
-        .select("id", "source_audio_interface_id", "destination_audio_interface_id")
-        .where("source_audio_interface_id", "in", this.state.loadedAudioInterfaces.map((audioInterface) => { return audioInterface.get("id"); }).toJS())
-        .on("fetch", (_event, _query, data) => {
+        .query('plumber', 'Config.Routing.LinkRule')
+        .select('id', 'source_audio_interface_id', 'destination_audio_interface_id')
+        .where('source_audio_interface_id', 'in', this.state.loadedAudioInterfaces.map((audioInterface) => { return audioInterface.get('id'); }).toJS())
+        .on('fetch', (_event, _query, data) => {
           if(this.isMounted()) {
             this.setState({
               loadedLinkRules: data
@@ -135,59 +133,58 @@ export default React.createClass({
   },
 
 
-  onClientDragStop: function(client, x, y) {
-    window.data.record("auth", "Client.Standalone", client.get("id"))
+  onClientDragStop(client, x, y) {
+    window.data.record('auth', 'Client.Standalone', client.get('id'))
       .update({
         extra: {
           electron: {
             diagram: {
-              x: x,
-              y: y
-            }
-          }
-        }
+              x,
+              y,
+            },
+          },
+        },
       });
   },
 
 
-  componentDidMount: function() {
+  componentDidMount() {
     this.loadClients();
   },
 
 
-  componentWillUnmount: function() {
-    if(this.linkRulesQuery) {
+  componentWillUnmount() {
+    if (this.linkRulesQuery) {
       this.linkRulesQuery.teardown();
       delete this.linkRulesQuery;
     }
 
-    if(this.audioInterfacesQuery) {
+    if (this.audioInterfacesQuery) {
       this.audioInterfacesQuery.teardown();
       delete this.audioInterfacesQuery;
     }
 
-    if(this.clientsQuery) {
+    if (this.clientsQuery) {
       this.clientsQuery.teardown();
       delete this.clientsQuery;
     }
   },
 
 
-  render: function() {
-    if(this.state.loadedClients === null || this.state.loadedAudioInterfaces === null || this.state.loadedLinkRules === null) {
+  render() {
+    if (this.state.loadedClients === null || this.state.loadedAudioInterfaces === null || this.state.loadedLinkRules === null) {
       return (
         <div>Loading</div>
       ); // FIXME use LoadingWidget
-
-    } else {
-
-      return (
-        <RoutingDiagramCanvas
-          clients={this.state.loadedClients}
-          audioInterfaces={this.state.loadedAudioInterfaces}
-          linkRules={this.state.loadedLinkRules}
-          onClientDragStop={this.onClientDragStop} />
-      );
     }
-  }
+
+    return (
+      <RoutingDiagramCanvas
+        clients={this.state.loadedClients}
+        audioInterfaces={this.state.loadedAudioInterfaces}
+        linkRules={this.state.loadedLinkRules}
+        onClientDragStop={this.onClientDragStop}
+      />
+    );
+  },
 });
