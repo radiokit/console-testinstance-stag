541f587d5d400fac11c8b4d03aabd9934106cdd5
Marcin Lewandowski <marcin@radiokit.org>
2016-05-20 13:12:06 +0200

    Fixed locales in library/file-repositories/index

---
+++ app/apps/library/file_repositories/IndexView.locale.en.js
@@ -0,0 +1,15 @@
+export default {
+  apps: {
+    library: {
+      file_repositories: {
+        index: {
+          header: "Library sections",
+
+          table: {
+            loading: "Fetching list of library sections",
+          },
+        },
+      },
+    }
+  }
+}
