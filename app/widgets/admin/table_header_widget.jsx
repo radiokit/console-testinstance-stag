ac95d8473b5f3bb4f6a94dc522ec56e30143c18e
Marcin Lewandowski <marcin@radiokit.org>
2016-06-08 15:20:39 +0200

    Added nowrap to the table header if it's sorted

--- app/widgets/admin/table_header_widget.jsx
+++ app/widgets/admin/table_header_widget.jsx
@@ -66,9 +66,9 @@ export default React.createClass({
           if(this.props.attributes[attribute].sortable === true) {
             if(this.props.sortedAttribute === attribute) {
               if(this.props.sortedDirection === "asc") {
-                label = <a onClick={this.onSort.bind(this, attribute, "desc")}>{label}&nbsp;<i className="mdi mdi-chevron-up" /></a>;
+                label = <a style={{"white-space": "nowrap"}} onClick={this.onSort.bind(this, attribute, "desc")}>{label}<i className="mdi mdi-chevron-up" /></a>;
               } else {
-                label = <a onClick={this.onSort.bind(this, attribute, "asc")}>{label}&nbsp;<i className="mdi mdi-chevron-down" /></a>;
+                label = <a style={{"white-space": "nowrap"}} onClick={this.onSort.bind(this, attribute, "asc")}>{label}<i className="mdi mdi-chevron-down" /></a>;
               }
 
             } else {
