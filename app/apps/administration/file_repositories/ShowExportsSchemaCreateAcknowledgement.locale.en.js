8f7b9655d7b3918d70254ec42d3ff59117708054
piotrekzyla <kontakt@piotrzyla.pl>
2016-06-20 14:09:34 +0200

    mixcloud export english locales

--- app/apps/administration/file_repositories/ShowExportsSchemaCreateAcknowledgement.locale.en.js
+++ app/apps/administration/file_repositories/ShowExportsSchemaCreateAcknowledgement.locale.en.js
@@ -1,3 +1,32 @@
 export default {
-
+  apps: {
+    administration: {
+      file_repositories: {
+        show: {
+          tabs: {
+            body: {
+              exports_schema: {
+                table: {
+                  index: {
+                    modals: {
+                      create: {
+                        acknowledgement: {
+                          instructions: {
+                            mixcloud: {
+                              intro: "Export has been added. Now you need to log in into the mixcloud account you want to use for publishing and confirm this application. Click the button below to do this:",
+                              action_open: "Go to MixCloud"
+                            }
+                          }
+                        }
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }
 };
