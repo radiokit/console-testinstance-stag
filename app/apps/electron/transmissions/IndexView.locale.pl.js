e8cb63d33f0b0912f970a36dee00cd73416f443d
piotrekzyla <kontakt@piotrzyla.pl>
2016-06-20 12:20:37 +0200

    ELEC-33  electron locales filled

--- app/apps/electron/transmissions/IndexView.locale.pl.js
+++ app/apps/electron/transmissions/IndexView.locale.pl.js
@@ -6,6 +6,7 @@ export default {
         errors: {
           no_devices: "Do tego konta nie przypisano żadnych urządzeń audio, przypisz najpierw jakieś urządzenia audio.",
           no_transmissions: "Obecnie nie odbywają się żadne transmisje",
+          no_clients: "Do tego konta nie przypisano żadnych klientów",
         },
       },
     }
