d1ebf969491a5958006991ab5812ac6f351d3b0f
Marcin Lewandowski <marcin@radiokit.org>
2016-08-23 16:46:47 +0200

    ELEC-40 ELEC-78 Display audio interfaces in electron's patchbay in 2 columns

--- app/apps/electron/patchbay/RoutingDiagramClientBox.jsx
+++ app/apps/electron/patchbay/RoutingDiagramClientBox.jsx
@@ -3,10 +3,16 @@ import React from 'react';
 import RoutingDiagramDimensions from './RoutingDiagramDimensions.js';
 import RoutingDiagramHelpers from './RoutingDiagramHelpers.js';
 
+import RoutingDiagramClientAudioInterface from './RoutingDiagramClientAudioInterface.jsx';
 
 const RoutingDiagramClientBox = (props) => {
-  let audioInterfacesOfClient = RoutingDiagramHelpers.getAudioInterfacesOfClient(props.client, props.audioInterfaces);
-  let audioInterfacesOfClientCount = audioInterfacesOfClient.count();
+  const audioInterfacesOfClient = RoutingDiagramHelpers.getAudioInterfacesOfClient(props.client, props.audioInterfaces);
+  const clientHeight = RoutingDiagramDimensions.getClientHeight(
+    Math.max(
+      audioInterfacesOfClient.filter(audioInterface => audioInterface.get('direction') === 'capture').count(),
+      audioInterfacesOfClient.filter(audioInterface => audioInterface.get('direction') === 'playback').count()
+    )
+  );
 
   return (
     <g>
@@ -16,7 +22,7 @@ const RoutingDiagramClientBox = (props) => {
         rx="4"
         ry="4"
         width={RoutingDiagramDimensions.getClientWidth()-2}
-        height={RoutingDiagramDimensions.getClientHeight(audioInterfacesOfClientCount)-2}
+        height={clientHeight}
         opacity="0.2"
         fill="#000000"
      />
@@ -28,7 +34,7 @@ const RoutingDiagramClientBox = (props) => {
         rx="2"
         ry="2"
         width={RoutingDiagramDimensions.getClientWidth()-2}
-        height={RoutingDiagramDimensions.getClientHeight(audioInterfacesOfClientCount)-2}
+        height={clientHeight}
         fill={props.selectedClient && props.selectedClient.get("id") === props.client.get("id") ? "#fff0a0" : "#006c90" }
         stroke="#2b323a"
         onClick={props.onClientBoxClick} />
@@ -48,42 +54,30 @@ const RoutingDiagramClientBox = (props) => {
 
 
       {audioInterfacesOfClient
+        .filter(audioInterface => audioInterface.get('direction') === 'capture')
         .map((audioInterface, index) => {
-          // Use audio interface box position as internal relative origin.
-          // Put it on the right edge of client's box if it's "capture"
-          // interface, or on the left otherwise.
-          let audioInterfaceX = (audioInterface.get("direction") === "capture" ? RoutingDiagramDimensions.getClientWidth() : 0) + RoutingDiagramDimensions.getAudioInterfaceWidth() / -2;
-          let audioInterfaceY = (index * (RoutingDiagramDimensions.getAudioInterfaceMargin() + RoutingDiagramDimensions.getAudioInterfaceHeight()) + RoutingDiagramDimensions.getAudioInterfaceMargin());
-
-          // Render box + interface's name
           return (
-            <g key={audioInterface.get("id")}
-              transform={`translate(${audioInterfaceX},${audioInterfaceY})`}>
-              <rect
-                style={{cursor: "pointer"}}
-                x="0"
-                y="0"
-                rx="2"
-                ry="2"
-                width={RoutingDiagramDimensions.getAudioInterfaceWidth()}
-                height={RoutingDiagramDimensions.getAudioInterfaceHeight()}
-                fill={props.selectedAudioInterface && props.selectedAudioInterface.get("id") === audioInterface.get("id") ? "#fff0a0" : "#fff"}
-                stroke="#2b323a"
-                onClick={props.onAudioInterfaceClick.bind(null, audioInterface)} />
-
-              <text
-                x={audioInterface.get("direction") === "capture" ? RoutingDiagramDimensions.getAudioInterfaceWidth() * -0.25 : RoutingDiagramDimensions.getAudioInterfaceWidth() * 1.25}
-                y="12"
-                textAnchor={audioInterface.get("direction") === "capture" ? "end" : "start"}
-                fontFamily="Roboto,Helvetica,Arial,sans"
-                fontSize="10px"
-                stroke="none"
-                fill={props.selectedClient && props.selectedClient.get("id") === props.client.get("id") ? "#000" : "#fff" }>
-                {audioInterface.get("name") === "" ? audioInterface.get("os_name") : audioInterface.get("name") }
-              </text>
-            </g>
+            <RoutingDiagramClientAudioInterface
+              key={audioInterface.get('id')}
+              index={index}
+              audioInterface={audioInterface}
+              onAudioInterfaceClick={props.onAudioInterfaceClick} />
           );
-      })}
+        }
+      )}
+
+      {audioInterfacesOfClient
+        .filter(audioInterface => audioInterface.get('direction') === 'playback')
+        .map((audioInterface, index) => {
+          return (
+            <RoutingDiagramClientAudioInterface
+              key={audioInterface.get('id')}
+              index={index}
+              audioInterface={audioInterface}
+              onAudioInterfaceClick={props.onAudioInterfaceClick} />
+          );
+        }
+      )}
     </g>
   );
 }
