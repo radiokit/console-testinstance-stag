f0d74d4e9a3000910617565e8f74e88cfe76d4b5
piotrekzyla <kontakt@piotrzyla.pl>
2016-08-30 14:32:47 +0200

    added journal.radiokitapp-stag to dev and stag environments

--- app/services/env.js
+++ app/services/env.js
@@ -12,6 +12,7 @@ function getEnv() {
       agenda: { baseUrl: 'https://agenda.radiokitapp-stag.org' },
       plumber: { baseUrl: 'https://plumber.radiokitapp-stag.org' },
       vault: { baseUrl: 'https://vault.radiokitapp-stag.org' },
+      journal: { baseUrl: 'https://journal.radiokitapp-stag.org' }
     },
     external: {
     },
