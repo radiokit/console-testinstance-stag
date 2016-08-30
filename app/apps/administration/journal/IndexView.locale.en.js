dd0be3cf8906ab6236e5937e8f7ac630d6eec8b2
piotrekzyla <kontakt@piotrzyla.pl>
2016-08-30 16:08:42 +0200

    CONS-33 created locales for journal

--- app/apps/administration/journal/IndexView.locale.en.js
+++ app/apps/administration/journal/IndexView.locale.en.js
@@ -1,82 +1,15 @@
 export default {
   apps: {
     administration: {
-      users: {
+      journal: {
         index: {
-          header: "Users",
-          actions: {
-            create: "Add user",
-            delete: "Delete selected users",
-          },
+          header: "Journal",
+
 
           table: {
-            loading: "Fetching list of users",
-            header: {
-              email: "e-mail",
-              first_name: "First name",
-              last_name: "Last name",
-              phone: "Phone",
-            },
+            loading: "Fetching list of events",
           },
 
-          modals: {
-            delete: {
-              header: "Delete user",
-              message: {
-                confirmation: "Are you sure that you want to delete %(count)s selected user(s)?",
-                acknowledgement: "Deleted %(count)s user(s).",
-                progress: "Deleting in progress…",
-              },
-              action: {
-                proceed: "Delete",
-                cancel: "Cancel",
-                close: "Close",
-              }
-            },
-
-            create: {
-              header: "Add user",
-              form: {
-                email: {
-                  label: "e-mail",
-                  hint: "Type user's e-mail address. It will serve as login.",
-                },
-                account_ids: {
-                  label: "Accounts",
-                  hint: "Select accounts to which this user belongs. Hold Ctrl while clicking to select/deselect multiple accounts.",
-                },
-                apps_available: {
-                  label: "Available applications",
-                  values: {
-                    electron: "Electron",
-                    library: "Library",
-                    administration: "Administration",
-                    broadcast: "Broadcast",
-                    freezer: "Freezer",
-                    infrastructure: "Infrastructure",
-                  },
-                  hint: "Select applications enabled for this user. Hold Ctrl while clicking to select/deselect multiple applications.",
-                },
-                locale: {
-                  label: "Language of the user interface",
-                  values: {
-                    en: "English",
-                    pl: "Polish",
-                  }
-                },
-              },
-
-              message: {
-                acknowledgement: "User has been added succesfully. Message with confirmation link was delivered on his/her mailbox.",
-              },
-
-              action: {
-                proceed: "Add user",
-                cancel: "Cancel",
-                close: "Close",
-              }
-            }
-          }
         },
       },
     }
--- app/apps/administration/journal/IndexView.locale.pl.js
+++ app/apps/administration/journal/IndexView.locale.pl.js
@@ -1,79 +1,15 @@
 export default {
   apps: {
     administration: {
-      users: {
+      journal: {
         index: {
-          header: "Użytkownikcy",
-          actions: {
-            create: "Dodaj użytkownika",
-            delete: "Usuń zaznaczonych użytkowników",
-          },
+          header: "Dziennik",
+
 
           table: {
-            loading: "Ładowanie listy użytkowników",
-            header: {
-              email: "e-mail",
-            },
+            loading: "Ładuję listę wydarzeń",
           },
 
-          modals: {
-            delete: {
-              header: "Usuń użytkownika",
-              message: {
-                confirmation: "Czy na pewno chcesz usunąć zaznaczonych użytkowników (%(count)s szt.)?",
-                acknowledgement: "Usunięto %(count)s użytkowników.",
-                progress: "Trwa usuwanie użytkowników…",
-              },
-              action: {
-                proceed: "Usuń",
-                cancel: "Anuluj",
-                close: "Zamknij",
-              }
-            },
-
-            create: {
-              header: "Dodaj użytkownika",
-              form: {
-                email: {
-                  label: "e-mail",
-                  hint: "Podaj e-mail użytkownika.",
-                },
-                account_ids: {
-                  label: "Konta",
-                  hint: "Wybierz konta do których należy ten użytkownik. Przytrzymaj Ctrl podczas klikania by zaznaczyć/odznaczyć wiele kont.",
-                },
-                apps_available: {
-                  label: "Dostępne aplikacje",
-                  values: {
-                    electron: "Electron",
-                    library: "Biblioteka",
-                    administration: "Administracja",
-                    broadcast: "Emisja",
-                    freezer: "Freezer",
-                    infrastructure: "Infrastruktura",
-                  },
-                  hint: "Wybierz aplikacje dostępne dla tego użytkownika. Przytrzymaj Ctrl podczas klikania by zaznaczyć/odznaczyć wiele aplikacji.",
-                },
-                locale: {
-                  label: "Język interfejsu użytkownika",
-                  values: {
-                    en: "Angielski",
-                    pl: "Polski",
-                  }
-                },
-              },
-
-              message: {
-                acknowledgement: "Pomyślnie dodano użytkownika. Na jego/jej skrzynkę e-mail została wysłana wiadomość z linkiem potwierdzającym.",
-              },
-
-              action: {
-                proceed: "Dodaj konto",
-                cancel: "Anuluj",
-                close: "Zamknij",
-              }
-            }
-          }
         },
       },
     }
