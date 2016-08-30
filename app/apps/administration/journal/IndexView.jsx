607da3f06920ccdabc9d34da3c415e122ff374ed
piotrekzyla <kontakt@piotrzyla.pl>
2016-08-30 14:30:01 +0200

    removing buildForm and ModifyQuery

--- app/apps/administration/journal/IndexView.jsx
+++ app/apps/administration/journal/IndexView.jsx
@@ -10,54 +10,9 @@ Counterpart.registerTranslations('pl', require('./IndexView.locale.pl.js'));
 export default React.createClass({
 
 
-  modifyIndexQuery(query) {
-    let availableUserAccountIds = this.context.availableUserAccounts.map((account) => { return account.get("id"); }).toJS();
-    let accountsCondition = ['accounts.id', 'in'].concat(availableUserAccountIds)
-
-    return query
-      .joins("accounts")
-      .where.apply(this, accountsCondition)
-      .order("email", "asc")
-  },
-
-
   buildAttributes() {
     return {
       email: { renderer: "string" },
-    };
-  },
-
-
-  buildForm() {
-    return {
-      email: {
-        type: 'email',
-        hint: true,
-        validators: {
-          presence: true,
-        },
-      },
-
-      locale: {
-        type: 'enum',
-        values: ['en', 'pl'],
-        validators: {
-          presence: true,
-        },
-      },
-
-      account_ids: {
-        type: 'set',
-        untranslated: true,
-        values: this.context.availableUserAccounts.reduce((reduction, account) => { return reduction.set(account.get("id"), account.get("name")); }, new Immutable.Map()).toJS(),
-        hint: true,
-      },
-
-      apps_available: {
-        type: 'set',
-        values: this.context.currentUser.get("apps_available").toJS(),
-        hint: true,
-      },
     };
   },
 
@@ -68,9 +23,9 @@ export default React.createClass({
         contentPrefix="apps.administration.journal"
         app="auth"
         model="Entry"
+        createEnabled: false
         attributes={this.buildAttributes()}
-        form={this.buildForm()}
-        indexQueryFunc={this.modifyIndexQuery} />
+        form={this.buildForm()} />
     );
   },
 });
