832d2ff52153405b519aee2cf29eed70d5ec5532
piotrekzyla <kontakt@piotrzyla.pl>
2016-08-30 14:54:39 +0200

    CONS-33 added journal to routing_helper

--- app/helpers/routing_helper.js
+++ app/helpers/routing_helper.js
@@ -54,6 +54,11 @@ export default {
           return '/apps/administration/users/index';
         },
       },
+        journal: {
+          index() {
+            return '/apps/administration/journal/index';
+          },
+      },
       file_repositories: {
         index() {
           return '/apps/administration/file_repositories/index';
