e51d42279cb331264c3a84507be15e2a26a1f859
Marcin Lewandowski <marcin@radiokit.org>
2016-12-16 16:41:50 +0100

    DEPO-169 Truncate long texts in Text table cell

--- app/widgets/admin/table_cell_text.jsx
+++ app/widgets/admin/table_cell_text.jsx
@@ -1,14 +1,29 @@
 import React from 'react';
+import _ from 'lodash';
 
 export default React.createClass({
   propTypes: {
     record: React.PropTypes.object.isRequired,
     value: React.PropTypes.string,
     attribute: React.PropTypes.string.isRequired,
+    truncateLength: React.PropTypes.number.isRequired,
+  },
+
+
+  getDefaultProps: function() {
+    return {
+      truncateLength: 60,
+    };
   },
 
 
   render: function() {
-    return (<span>{this.props.value}</span>);
+    const truncatedValue = _.truncate(this.props.value, {
+      length: this.props.truncateLength,
+      separator: /\.,? +/,
+      omission: '…',
+    });
+
+    return (<span>{truncatedValue}</span>);
   }
 });
