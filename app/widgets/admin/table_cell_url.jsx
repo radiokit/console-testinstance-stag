6c97160fbef51d55646c7849358f7dc7dacde70f
Marcin Lewandowski <marcin@radiokit.org>
2017-04-10 17:21:38 +0200

    Upgrades to the table URL rendeder

--- app/widgets/admin/table_cell_url.jsx
+++ app/widgets/admin/table_cell_url.jsx
@@ -1,14 +1,40 @@
 import React from 'react';
+import _ from 'lodash';
 
 export default React.createClass({
   propTypes: {
     record: React.PropTypes.object.isRequired,
     value: React.PropTypes.string,
     attribute: React.PropTypes.string.isRequired,
+    selectable: React.PropTypes.bool.isRequired,
+    truncateLength: React.PropTypes.number.isRequired,
+  },
+
+
+  getDefaultProps: function() {
+    return {
+      truncateLength: 40,
+      selectable: false,
+    };
   },
 
 
   render: function() {
-    return (<span>{this.props.value}</span>);
+    const truncatedValue = _.truncate(this.props.value, {
+      length: this.props.truncateLength,
+      omission: '…',
+    });
+
+    let titleValue = null;
+    if(this.props.value !== truncatedValue) {
+      titleValue = this.props.value;
+    }
+
+    if(this.props.selectable) {
+      return (<span className="selectable" title={titleValue}><a href={this.props.value}>{truncatedValue}</a></span>);
+
+    } else {
+      return (<span title={titleValue}><a href={this.props.value}>{truncatedValue}</a></span>);
+    }
   }
 });
