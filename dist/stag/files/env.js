17b525254a9770a0ee900ea45e9aa9d770ab104f
Adam Winiarczyk <adam.winiarczyk@swmansion.com>
2017-04-10 12:43:56 +0200

    Add circumstances endpoint to stag envs

--- dist/stag/files/env.js
+++ dist/stag/files/env.js
@@ -4,13 +4,14 @@ window.ENV = {
     baseUrl: "https://auth.radiokitapp-stag.org"
   },
   apps: {
-    "jungle"   : { baseUrl: "https://jungle.radiokitapp-stag.org" },
-    "medium"   : { baseUrl: "https://medium.radiokitapp-stag.org" },
-    "auth"     : { baseUrl: "https://auth.radiokitapp-stag.org" },
-    "agenda"   : { baseUrl: "https://agenda.radiokitapp-stag.org" },
-    "plumber"  : { baseUrl: "https://plumber.radiokitapp-stag.org" },
-    "vault"    : { baseUrl: "https://vault.radiokitapp-stag.org" },
-    "journal"  : { baseUrl: "https://journal.radiokitapp-stag.org" },
+    "jungle"        : { baseUrl: "https://jungle.radiokitapp-stag.org" },
+    "medium"        : { baseUrl: "https://medium.radiokitapp-stag.org" },
+    "auth"          : { baseUrl: "https://auth.radiokitapp-stag.org" },
+    "agenda"        : { baseUrl: "https://agenda.radiokitapp-stag.org" },
+    "plumber"       : { baseUrl: "https://plumber.radiokitapp-stag.org" },
+    "vault"         : { baseUrl: "https://vault.radiokitapp-stag.org" },
+    "journal"       : { baseUrl: "https://journal.radiokitapp-stag.org" },
+    "circumstances" : { baseUrl: 'https://circumstances.radiokitapp-stag.org' },
   },
   external: {
   },
