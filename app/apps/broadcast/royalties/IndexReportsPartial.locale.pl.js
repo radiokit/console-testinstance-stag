1a6bb1cc10f25fddd55a97ad1eb6987f7603f11b
Marcin Lewandowski <marcin@radiokit.org>
2017-04-20 21:52:10 +0200

    Added UI for generating royalties reports, radiokit/product-horn#5

---
+++ app/apps/broadcast/royalties/IndexReportsPartial.locale.pl.js
@@ -0,0 +1,25 @@
+export default {
+  apps: {
+    broadcast: {
+      royalties: {
+        reports: {
+          form: {
+            month: {
+              label: "Miesiąc",
+            },
+            year: {
+              label: "Rok",
+            },
+            template: {
+              label: "Wzór",
+              values: {
+                stoart: "STOART (Polska, Związek Artystów Wykonawców STOART)",
+              },
+            },
+          },
+          submit: "Wygeneruj raport",
+        },
+      },
+    },
+  },
+};
