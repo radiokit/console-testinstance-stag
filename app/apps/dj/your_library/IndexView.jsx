346f4cc15e28ccddb484b0d526024486fa5e7b80
Piotr Zyla <kontakt@piotrzyla.pl>
2017-05-15 17:16:50 +0200

    copypasted library to DjYourLibrary #29

--- app/apps/dj/your_library/IndexView.jsx
+++ app/apps/dj/your_library/IndexView.jsx
@@ -1,11 +1,65 @@
 import React from 'react';
+import Counterpart from 'counterpart';
+
+import Index from '../../../widgets/admin/crud/index_widget.jsx';
+
+
+Counterpart.registerTranslations('en', require('./IndexView.locale.en.js'));
+Counterpart.registerTranslations('pl', require('./IndexView.locale.pl.js'));
+
 
 export default React.createClass({
+  contextTypes: {
+    currentAccount: React.PropTypes.object.isRequired,
+    userAccount: React.PropTypes.object.isRequired,
+  },
+
+  modifyIndexQuery(query) {
+    return query
+      .where('references', 'deq', 'user_account_id', this.context.currentAccount.get('id'))
+      .order('name', 'asc');
+  },
+
+  buildAttributes() {
+    return {
+      name: { renderer: 'string' },
+      files_size_total: { renderer: 'filesize' },
+      files_count: { renderer: 'integer' },
+      user_account: { renderer: 'scope-user-account' },
+    };
+  },
+
+  buildForm() {
+    return {
+      name: {
+        type: 'string',
+        hint: true,
+        validators: {
+          presence: true,
+        },
+      },
+      user_account: {
+        type: 'scope-user-account',
+        validators: {
+          presence: true,
+        },
+      },
+    };
+  },
 
   render() {
-      return (<div><h1>What's Up dj?</h1>
-    <h2>Here you'll find your library soon!</h2>
-      </div>);
-
-    }
+    return (
+      <Index
+        type="list"
+        contentPrefix="apps.dj.your_library"
+        app="vault"
+        model="Data.Record.Repository"
+        attributes={this.buildAttributes()}
+        form={this.buildForm()}
+        indexQueryFunc={this.modifyIndexQuery}
+        createEnabled={false}
+        deleteEnabled={false}
+      />
+    );
+  },
 });
---
+++ app/apps/dj/your_library/IndexView.locale.pl.js
@@ -0,0 +1,21 @@
+export default {
+  apps: {
+    library: {
+      file_repositories: {
+        index: {
+          header: 'Repozytoria plików',
+
+          table: {
+            loading: 'Pobieranie repozytoriów plików',
+          },
+        },
+        show: {
+          sidebar: {
+            all_tags: 'Wszystkie',
+            edit_tag_metadata: 'Edytuj metadane',
+          },
+        },
+      },
+    },
+  },
+};
