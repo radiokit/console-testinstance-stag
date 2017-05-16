78bd9a5c4da7051bdb98dd100f3023ea222fbd64
piotrekzyla <kontakt@piotrzyla.pl>
2017-05-16 16:11:32 +0200

    4 buttons changing states #36

--- app/apps/dj/go_live/IndexView.jsx
+++ app/apps/dj/go_live/IndexView.jsx
@@ -1,10 +1,39 @@
 import React from 'react';
 
+
 export default React.createClass({
 
+    getInitialState: function () {
+      return {live: false}
+    },
+    goLive: function () {
+      this.setState({live: true})
+      console.log(this.state)
+    },
+    stopBroadcast: function (){
+      this.setState({live: false})
+      console.log(this.state)
+    },
+    record: function () {
+      this.setState({recording: true})
+      console.log(this.state)
+    },
+    stopRecording: function (){
+      this.setState({recording: false})
+      console.log(this.state)
+    },
+
+
   render() {
-      return (<div><h1>What's Up dj?</h1>
-    <h2>Here you'll be able to go live soon!</h2>
+      return (<div>
+        <div>
+        <button onClick={this.goLive}>Go Live</button>
+        <button onClick={this.stopBroadcast}>Stop Broadcast</button>
+        </div>
+        <div>
+        <button onClick={this.record}>Record Set</button>
+        <button onClick={this.stopRecording}>Stop Recording</button>
+        </div>
       </div>);
 
     }
